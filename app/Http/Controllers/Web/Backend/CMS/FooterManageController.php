c5c42801202ffe032150da82bf57604bbcda0168
code-arif <arifulislam6460@gmail.com>
2025-11-13 11:00:40 +0600

    chore(cms): add debug exit to footer index method

--- app/Http/Controllers/Web/Backend/CMS/FooterManageController.php
+++ app/Http/Controllers/Web/Backend/CMS/FooterManageController.php
@@ -14,6 +14,7 @@ class FooterManageController extends Controller
     public function index()
     {
         $data = Footer::first() ?? new Footer();
+        return 1;exit();
         return view('backend.layouts.cms.partials.footer', compact('data'));
     }
 
