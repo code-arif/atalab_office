17e5edec5b614cbb5a76d3c8b7dce367f1e68d88
code-arif <arifulislam6460@gmail.com>
2025-11-24 09:36:32 +0600

    fix: remove debug statement from footer update method

--- app/Http/Controllers/Web/Backend/CMS/FooterManageController.php
+++ app/Http/Controllers/Web/Backend/CMS/FooterManageController.php
@@ -20,7 +20,6 @@ class FooterManageController extends Controller
     // Handle footer update
     public function update(Request $request)
     {
-        dd($request->all());
         $request->validate([
             'logo' => 'nullable|max:2048',
             'business_name' => 'nullable|string|max:255',
