6a65e9ad897d196c00987937d76a40018612eef7
code-arif <arifulislam6460@gmail.com>
2025-12-27 13:55:30 +0600

    founder statement comment line  change

--- resources/views/backend/layouts/cms/home/founder_statement.blade.php
+++ resources/views/backend/layouts/cms/home/founder_statement.blade.php
@@ -69,7 +69,7 @@
                                             @enderror
                                         </div>
 
-                                        {{-- slected name --}}
+                                        {{-- image --}}
                                         <div class="row">
                                             <div class="col-md-12">
                                                 <div class="form-group">
